449052855b7428c7824fbf2daadc5fd6b34e2546
komphet <komphet@kmi.tl>
2018-02-07 11:48:25 +0700

    update

--- lab5_2.c
+++ lab5_2.c
@@ -2,34 +2,47 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+
+#define clear_buffer() while (getchar() != '\n')
 int main()
 {
    int input;
    pid_t cpid;
    do{
-	 printf("=================================================\n");
-     printf("a. ls\nb. ps\nc. who\nq. exit\n");
-     input = getchar();
-     
-     switch(input){
-     	case 'a': 
-     		cpid = fork();
-     		if(cpid == 0){
-     			execlp("/bin/ls", "ls", NULL);
-     			break;
-     			exit(0);
-     		}
-     		
-     	case 'b': break;
-     	case 'c': break;
-     	case 'q': 
-     		exit(0);
-			break;
-     	default:
-     		printf("You are idiot!!! WTFFFFF!!! Just read command idiot!!\n");
-     }
-     wait(NULL);
-     printf("=================================================\n");
+		 printf("=================================================\n");
+	     printf("a. ls\nb. ps\nc. who\nq. exit\n");
+	     input = getchar();
+	     clear_buffer();
+	     
+	     switch(input){
+	     	case 'a': 
+	     		cpid = fork();
+	     		if(cpid == 0){
+	     			execlp("/bin/ls", "ls", NULL);
+	     			exit(0);
+	     		}
+	     		break;
+	     	case 'b':
+	     		cpid = fork();
+	     		if(cpid == 0){
+	     			execlp("/bin/ps", "ps", NULL);
+	     			exit(0);
+	     		}
+	     		break;
+	     	case 'c': 
+	     		cpid = fork();
+	     		if(cpid == 0){
+	     			execlp("/bin/who", "who", NULL);
+	     			exit(0);
+	     		}
+	     		break;
+	     	case 'q': 
+				break;
+	     	default:
+	     		printf("You are idiot!!! WTFFFFF!!! Just read command idiot!!\n");
+	     }
+	     wait(NULL);
+	 
    } while(input != 'q');   
 
    printf("Buy~~~~~!\n");
